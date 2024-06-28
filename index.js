d2e733ce031d610073313b2e713a9d82700761c4
David Lucas <davidlucaskl@gmail.com>
2024-06-28 12:58:10 -0300

    start project

---
+++ index.js
@@ -0,0 +1,155 @@
+const express = require("express");
+const exps = require("express-handlebars");
+const mysql = require("mysql");
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+
+app.engine("handlebars", exps.engine());
+app.set("view engine", "handlebars");
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.post("/tasks/inserttask", (req, res) => {
+  const title = req.body.title;
+  const description = req.body.description;
+  const priority = req.body.priority;
+  const finish = false;
+
+  const sql = `INSERT INTO tasks(title, description, priority, finish) VALUES('${title}', '${description}', '${priority}', '${finish}')`;
+
+  conn.query(sql, function (err) {
+    if (err) {
+      console.log(err);
+    }
+    res.redirect("/tasks");
+  });
+});
+
+const conn = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "tasklist",
+});
+
+app.get("/tasks", (req, res) => {
+  const sql = `SELECT * FROM tasks WHERE finish = '${0}'`;
+  conn.query(sql, function (err, data) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    const tasks = data;
+    res.render("tasks", { tasks });
+  });
+});
+
+app.get("/tasks/finish", (req, res) => {
+  const sql = `SELECT * FROM tasks WHERE finish = 1`;
+  conn.query(sql, function (err, data) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    const tasks = data;
+    res.render("finish", { tasks });
+  });
+});
+
+app.post("/tasks/finish/:id", (req, res) => {
+  const id = req.params.id;
+  const sql = `UPDATE tasks SET finish = 1 WHERE id = ${id}`;
+  conn.query(sql, function (err, data) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    res.json({ success: true });
+  });
+});
+
+app.post("/tasks/refactor/:id", (req, res) => {
+  const id = req.params.id;
+  const sql = `UPDATE tasks SET finish = 0 WHERE id = ${id}`;
+  conn.query(sql, function (err, data) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    res.json({ success: true });
+  });
+});
+
+app.get("/task/:id", (req, res) => {
+  const id = req.params.id;
+
+  const sql = `SELECT * FROM tasks WHERE id = ${id}`;
+  conn.query(sql, function (err, data) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    const task = data[0];
+    res.render("task", { task });
+  });
+});
+
+app.delete("/task/delete/:id", (req, res) => {
+  const id = req.params.id;
+  const sql = `DELETE FROM tasks WHERE id = ${id}`;
+
+  conn.query(sql, (err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send("Erro ao excluir a tarefa");
+      return;
+    }
+    res.status(200).send("Tarefa excluída com sucesso");
+  });
+});
+
+app.get("/tasks/edit/:id", (req, res) => {
+  const id = req.params.id;
+
+  const sql = `SELECT * FROM tasks WHERE id = ${id}`;
+  conn.query(sql, function (err, data) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    const task = data[0];
+    res.render("edittask", { task });
+  });
+});
+
+app.post("/tasks/updatetask", (req, res) => {
+  const id = req.body.id;
+  const title = req.body.title;
+  const description = req.body.description;
+  const priority = req.body.priority;
+
+  const sql = `UPDATE tasks SET title='${title}', description='${description}', priority='${priority}' WHERE id = ${id}`;
+
+  conn.query(sql, function (err) {
+    if (err) {
+      console.log(err);
+    }
+    res.redirect("/tasks");
+  });
+});
+
+conn.connect(function (err) {
+  if (err) {
+    console.log(err);
+  }
+  console.log("Conectou ao MySQL");
+  app.listen(3000);
+});
